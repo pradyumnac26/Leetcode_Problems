013086bd0193161aa76080c0ed1bb9d6fd2758b7
Praddy <69765504+pradyumnac26@users.noreply.github.com>
2022-02-26 22:33:04 +0530

    Time: 8 ms (82.68%), Space: 7.4 MB (8.23%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
+       /* sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         if(s==t)
         {
@@ -11,6 +11,22 @@ public:
         {
             return false;
         }
+        */
+        if (s.size()!=t.size()) return false;
         
+		unordered_map <char, int> umap;
+        for (char c:s) umap[c]++;
+        for(char c:t) {
+            umap[c]--;
+            
+        }
+        for(auto it:umap)
+        {
+            if(it.second) return false;
+        }
+        
+        return true;
+                    
     }
+    
 };
