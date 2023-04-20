07edc76f4a09ebfc4e9da94017336a6ad44a09b6
Praddy <69765504+pradyumnac26@users.noreply.github.com>
2023-04-20 14:57:18 +0530

    Time: 43 ms (85.14%), Space: 14 MB (67.10%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
@@ -1,15 +1,13 @@
 class Solution:
-    def maximumDifference(self, nums: List[int]) -> int:
-        mini = nums[0]
-        maxi = -1
-        for i in range(1, len(nums)) :
-            if (nums[i] - mini > maxi) :
-                maxi = nums[i] - mini
-                
-            if nums[i] < mini :
-                mini = nums[i]
-        if maxi > 0 :
-            return maxi
-        else : 
-            return -1
+    def maxProfit(self, prices: List[int]) -> int:
+        max_diff = 0
+        min_element = prices[0]
+      
+        for i in range( 1, len(prices) ):
+            if (prices[i] - min_element > max_diff):
+                max_diff = prices[i] - min_element
+      
+            if (prices[i] < min_element):
+                min_element = prices[i]
+        return max_diff
         
