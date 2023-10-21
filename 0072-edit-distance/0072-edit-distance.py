b095d3dfeb8bccd25487690fdd220cd62ecd5b4b
Praddy <69765504+pradyumnac26@users.noreply.github.com>
2023-10-20 22:40:57 -0600

    Time: 139 ms (26.70%), Space: 19.9 MB (56.83%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        l1=len(word1)
+        l2=len(word2)
+        dp=[[0]*(l2+1) for i in range(l1+1)]
+        cnt=0
+        for i in range(l1+1):
+            for j in range(l2+1):
+                if i==0:
+                    dp[i][j]=j
+                elif j==0:
+                    dp[i][j]=i
+                elif word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])+1
+        return dp[-1][-1]
+        
