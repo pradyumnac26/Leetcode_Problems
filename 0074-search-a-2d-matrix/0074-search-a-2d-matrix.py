6a0bc37a44d987bbe42d83109e7606d4796643c4
Praddy <69765504+pradyumnac26@users.noreply.github.com>
2024-06-01 12:54:36 -0600

    Time: 39 ms (91.14%), Space: 17.3 MB (7.27%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,31 +5,21 @@ class Solution:
         #         return True 
         # else :
         #     return False
+        n = len(matrix)
+        m = len(matrix[0])
+        low = 0 
+        high = n*m-1
         
-        ROWS,COLS = len(matrix), len(matrix[0])
-        top,bot = 0 , ROWS - 1
-        while top <= bot :
-            row = (top + bot)//2
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0] :
-                bot = row - 1
-            else :
-                break 
-        if not (top <= bot ):
-            return False 
-        row = (top + bot)//2
-        l,r = 0 ,COLS-1
-        while l<=r :
-            m = (l+r)//2
-            if target > matrix[row][m]:
-                l = m+1
-            elif target < matrix[row][m]:
-                r = m-1
-            else :
-                return True
-        return False
+        while low <= high :
+            mid = (low + high)//2
+            row = mid//m 
+            col = mid % m
+            
+            if  matrix[row][col] == target : return True  
+            elif matrix[row][col] <= target : low = mid +1
+            else : high = mid -1
                 
+        return False 
             
         
             
